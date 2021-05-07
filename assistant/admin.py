1a4a58714bb3e89eeaa7596a0d83f6b148c95efa
Shamsuddin Ahmed <shamsuddin.codesmith@gmail.com>
2021-05-07 11:18:17 +0600

    Assistant view 50%

--- assistant/admin.py
+++ assistant/admin.py
@@ -1,10 +1,60 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Patient, PatientInfos, MediaVideo, MediaImage, MediaDocument, Assign
+from .models import (
+    Patient,
+    PatientInfos,
+    MediaVideo,
+    MediaImage,
+    MediaDocument,
+    Assign
+)
 
-admin.site.register(Patient)
-admin.site.register(PatientInfos)
+from django import forms
+
+
+class PatientAdminForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = "__all__"
+
+
+class PatientAdmin(admin.ModelAdmin):
+    form = PatientAdminForm
+    list_display = [
+        "diagnosis",
+        "date_of_discharge",
+        "phone",
+        "date_of_admission",
+        "age",
+        "address",
+        "prof_surgeon_consultant",
+        "name",
+        "sex",
+    ]
+    readonly_fields = [
+        "date_of_admission",
+    ]
+
+
+admin.site.register(Patient, PatientAdmin)
+
+
+class PatientInfosAdminForm(forms.ModelForm):
+    class Meta:
+        model = PatientInfos
+        fields = "__all__"
+
+
+class PatientInfosAdmin(admin.ModelAdmin):
+    form = PatientAdminForm
+    list_display = '__all__'
+    readonly_fields = [
+
+    ]
+
+
+admin.site.register(PatientInfos, PatientInfosAdmin)
+
 admin.site.register(Assign)
 admin.site.register(MediaVideo)
 admin.site.register(MediaImage)
